c60350f27d9d416dc82660abe4f30b90df740545
Stanley2025 <ssgaryeazohn@gmail.com>
2025-08-24 19:05:03 +0530

    feat: Complete PWA implementation for CineSeek app

--- alx-movie-app/next.config.ts
+++ alx-movie-app/next.config.ts
@@ -1,17 +1,18 @@
+import withPWAInit from "@ducanh2912/next-pwa";
+
 /** @type {import('next').NextConfig} */
+
+const withPWA = withPWAInit({
+  dest: 'public'
+})
+
 const nextConfig = {
   reactStrictMode: true,
-  
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'm.media-amazon.com', 
-        port: '',
-        pathname: '/images/**', 
-      },
-    ],
+    domains: ['m.media-amazon.com'],
   },
 };
 
-module.exports = nextConfig;
+export default withPWA({
+  ...nextConfig
+})
